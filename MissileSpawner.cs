486f61ea3ae3a8919b972ee7550f3fb6dee22fdf
Xawe <dreamxawe@hotmail.com>
2020-12-14 19:20:37 -0300

    comentario em classe missileSpawner

--- MissileSpawner.cs
+++ MissileSpawner.cs
@@ -8,11 +8,17 @@ using System.Threading.Tasks;
 
 namespace Defenders
 {
+    /// <summary>
+    /// Classe responsável por criar as regras para espalhar os misseis.
+    /// TODO - implementar algoritimo que permita de forma randomica, distrbuir 1 ou mais misseis em intervalos
+    /// de tempo também aleatórios.
+    /// Os misseis devem distribuidos no eixo Y (0) e X (random) de forma a que o angulo da descida nao colida com as laterais.
+    /// OU em caso de colisão, definir se o missel muda de lado da tela ou se explore, disparando 1 ou mais fragmentos em outros angulos
+    /// </summary>
     public class MissileSpawner
     {
         private TimeSpan elapsedSpawn;
         private List<Objects.Missile> missileList;        
-
         private Game _game;
         
         public MissileSpawner(Game game)
