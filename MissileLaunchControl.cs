f6cb12ed765bb6b141e624c937f16479fb0cc2f8
Xawe <dreamxawe@hotmail.com>
2020-12-15 21:44:26 -0300

    cleanup

--- MissileLaunchControl.cs
+++ MissileLaunchControl.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Defenders
 {
@@ -18,19 +14,19 @@ namespace Defenders
     public class MissileLaunchControl
     {
         private TimeSpan elapsedSpawn;
-        private List<Objects.Missile> missileList;        
+        private List<Objects.Missile> missileList;
         private Game _game;
-        
-        
+
+
         public MissileLaunchControl(Game game)
         {
             _game = game;
             missileList = new List<Objects.Missile>();
-            elapsedSpawn = TimeSpan.Zero;                        
+            elapsedSpawn = TimeSpan.Zero;
         }
 
         public ValueTuple<bool, Objects.Missile> LaunchMissile(GameTime gameTime)
-        {            
+        {
             if (Math.Round(gameTime.TotalGameTime.TotalSeconds) > Math.Round(elapsedSpawn.TotalSeconds))
             {
                 elapsedSpawn = gameTime.TotalGameTime;
@@ -38,7 +34,7 @@ namespace Defenders
             }
             return new ValueTuple<bool, Objects.Missile>(false, null);
         }
-       
+
         private float DefineHorizontalLauchPoint(Game game, GameTime gameTime)
         {
             var varNumber = (gameTime.TotalGameTime.Milliseconds / (gameTime.TotalGameTime.Seconds + 1) * Math.PI);
--- Objects/Missile.cs
+++ Objects/Missile.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Defenders.Objects
 {
@@ -13,8 +10,8 @@ namespace Defenders.Objects
     /// 
     /// </summary>
     public class Missile
-    {        
-        public Texture2D Texture { get; set; }       
+    {
+        public Texture2D Texture { get; set; }
         public float Angle { get; set; }
         public float SpeedFactor { get; set; }
 
@@ -28,11 +25,11 @@ namespace Defenders.Objects
         public Missile(Game game, Vector2 position)
         {
             if (SpeedFactor == 0) SpeedFactor = 255f;
-            velocity = new Vector2(0);            
+            velocity = new Vector2(0);
             Texture = game.Content.Load<Texture2D>("Ship");
-            spriteOrigin = new Vector2(Texture.Width/2, Texture.Height/2);
+            spriteOrigin = new Vector2(Texture.Width / 2, Texture.Height / 2);
             this.position = position;
-            Angle = 2.8f ;            
+            Angle = 2.8f;
         }
         /// <summary>
         /// Método responsável por executar os calculos e atualizações do objeto
@@ -44,12 +41,12 @@ namespace Defenders.Objects
             Vector2 up = new Vector2(0f, -1f);
             Matrix rotMatrix = Matrix.CreateRotationZ(Angle);
 
-            if(acceleration == Vector2.Zero)
+            if (acceleration == Vector2.Zero)
             {
-                acceleration = Vector2.Transform(up, rotMatrix) * SpeedFactor;             
-            }            
+                acceleration = Vector2.Transform(up, rotMatrix) * SpeedFactor;
+            }
             velocity += acceleration * deltaTime * deltaTime;
-            position += velocity * deltaTime;            
+            position += velocity * deltaTime;
         }
 
         /// <summary>
